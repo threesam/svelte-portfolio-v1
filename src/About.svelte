a3082f731dd2e026306796dff170c1c8499ed354
salvatoredangelo <salvatoredangelo@protonmail.com>
2020-05-28 22:07:20 -0400

    toggle between about card title over background and card content

--- src/About.svelte
+++ src/About.svelte
@@ -3,20 +3,32 @@
 
   const tiles = [
     {
+      toggled: false,
       title: 'bio',
       src: '/images/eyeGrapefruit.png',
+      content:
+        'Ad ut veniam aute Lorem ad ullamco et sit do pariatur qui mollit. Quis minim cupidatat irure nisi dolor irure do culpa minim ullamco laborum non. Dolor tempor sint est et qui laboris eu aute sunt excepteur excepteur ipsum eu. Elit consequat eiusmod duis proident anim deserunt dolore consectetur veniam sint elit Lorem sit. Exercitation est irure id velit non proident velit Lorem nostrud. Ullamco sit est adipisicing anim laborum duis et adipisicing consequat elit labore aute. Commodo ut cillum culpa cillum et ipsum commodo reprehenderit non qui enim.',
     },
     {
+      toggled: false,
       title: 'fam',
-      src: '/images/darkommute.png',
+      src: '/images/eyeGrapefruit.png',
+      content:
+        'Ad ut veniam aute Lorem ad ullamco et sit do pariatur qui mollit. Quis minim cupidatat irure nisi dolor irure do culpa minim ullamco laborum non. Dolor tempor sint est et qui laboris eu aute sunt excepteur excepteur ipsum eu. Elit consequat eiusmod duis proident anim deserunt dolore consectetur veniam sint elit Lorem sit. Exercitation est irure id velit non proident velit Lorem nostrud. Ullamco sit est adipisicing anim laborum duis et adipisicing consequat elit labore aute. Commodo ut cillum culpa cillum et ipsum commodo reprehenderit non qui enim.',
     },
     {
+      toggled: false,
       title: 'music',
       src: '/images/eyeGrapefruit.png',
+      content:
+        'Ad ut veniam aute Lorem ad ullamco et sit do pariatur qui mollit. Quis minim cupidatat irure nisi dolor irure do culpa minim ullamco laborum non. Dolor tempor sint est et qui laboris eu aute sunt excepteur excepteur ipsum eu. Elit consequat eiusmod duis proident anim deserunt dolore consectetur veniam sint elit Lorem sit. Exercitation est irure id velit non proident velit Lorem nostrud. Ullamco sit est adipisicing anim laborum duis et adipisicing consequat elit labore aute. Commodo ut cillum culpa cillum et ipsum commodo reprehenderit non qui enim.',
     },
     {
+      toggled: false,
       title: 'coding',
-      src: '/images/darkommute.png',
+      src: '/images/eyeGrapefruit.png',
+      content:
+        'Ad ut veniam aute Lorem ad ullamco et sit do pariatur qui mollit. Quis minim cupidatat irure nisi dolor irure do culpa minim ullamco laborum non. Dolor tempor sint est et qui laboris eu aute sunt excepteur excepteur ipsum eu. Elit consequat eiusmod duis proident anim deserunt dolore consectetur veniam sint elit Lorem sit. Exercitation est irure id velit non proident velit Lorem nostrud. Ullamco sit est adipisicing anim laborum duis et adipisicing consequat elit labore aute. Commodo ut cillum culpa cillum et ipsum commodo reprehenderit non qui enim.',
     },
   ]
 </script>
--- src/AboutTile.svelte
+++ src/AboutTile.svelte
@@ -1,23 +1,58 @@
 <script>
   export let title
   export let src
+  export let content
+
+  let card = { toggled: false }
+
+  const toggle = () => {
+    card.toggled = !card.toggled
+  }
 </script>
 
 <style>
   div {
     display: grid;
     place-content: center;
+  }
+  #not-toggled {
     background-size: cover;
     background-position: center;
+    filter: grayscale(100%);
+  }
+  #not-toggled:hover {
+    filter: grayscale(30%);
+    border: 0.25rem solid black;
+  }
+
+  #toggled {
+    background-image: unset;
+    background: black;
+    color: white;
+    padding: 1rem;
+    font-size: 1.5rem;
+    line-height: 1.9rem;
+  }
+  #toggled:hover {
+    filter: none;
+    border: none;
   }
   h2 {
     margin-bottom: 1rem;
-    font-size: 3rem;
+    font-size: 1rem;
     color: white;
     text-shadow: 2px 2px 15px white;
   }
 </style>
 
-<div style="background-image: url({src});">
-  <h2>{title}</h2>
-</div>
+{#if !card.toggled}
+  <div id="not-toggled" style="background-image: url({src});" on:click={toggle}>
+    <h2>{title}</h2>
+  </div>
+{/if}
+
+{#if card.toggled}
+  <div id="toggled" on:click={toggle}>
+    <p>{content}</p>
+  </div>
+{/if}
